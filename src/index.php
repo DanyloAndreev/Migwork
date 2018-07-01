b383dd22c8d14f71246aa8d594da7cc556aef69c
DanyloAndreiev <dan4ik1507@gmail.com>
2018-07-01 12:42:21 +0300

    directory index file

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
-<?php require_once ('/tpl/meta.tpl');?>
-<?php require_once ('/tpl/header.tpl');?>
-<?php require_once ('/tpl/greeting.tpl')?>
-<?php require_once ('/tpl/footer.tpl')?>
-<?php require_once ('/tpl/end.tpl')?>
+<?php
+require_once ('tpl/meta.tpl');
+require_once ('tpl/header.tpl');
+require_once ('tpl/greeting.tpl');
+require_once ('tpl/footer.tpl');
+require_once ('tpl/end.tpl');
+?>
